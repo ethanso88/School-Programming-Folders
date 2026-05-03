58fce4f92db005e42fabe25e081dc632972d0fa4
ethanso88 <ethanso788@gmail.com>
2026-05-03 01:59:26 -0400

    Describe what you changed

--- ICS2O1/Unit 1/Assignment/EthanQ1.py
+++ ICS2O1/Unit 1/Assignment/EthanQ1.py
@@ -13,4 +13,4 @@ average = (num1 + num2 + num3)/3
 #output
 print(average, 'is your average of the three numbers')
 
-#used variables, inputs typecasting, and calculations for this program. 
+#used variables, inputs typecasting, and calculations for this program
